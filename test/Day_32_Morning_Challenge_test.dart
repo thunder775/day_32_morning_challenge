aa2fea392c54ac0c2de5c7bdfe123d9a91d686d7
kapil jhajhria <jhajhria44@gmail.com>
2019-11-18 09:29:06 +0530

    test cases by kapil

--- test/Day_32_Morning_Challenge_test.dart
+++ test/Day_32_Morning_Challenge_test.dart
@@ -1,5 +1,13 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
-
+  Shiritori shiritori= Shiritori();
+  test("test case 1", () {
+//    expect(shiritori.restart, shiritori.wordsList.isEmpty);
+    expect(shiritori.wordsList.isEmpty, true);
+    expect(shiritori.play('hello'), true);
+    expect(shiritori.wordsList.isEmpty, false); // if the word is not preset in the list and valid then add it to the list
+  });
 }
+
