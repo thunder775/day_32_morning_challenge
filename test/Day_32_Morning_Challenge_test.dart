9784f1c2459d1fad5791f5666bb14abef0fa083c
kapil jhajhria <jhajhria44@gmail.com>
2019-11-18 09:31:59 +0530

    another test case added - kapil

--- test/Day_32_Morning_Challenge_test.dart
+++ test/Day_32_Morning_Challenge_test.dart
@@ -8,6 +8,7 @@ void main() {
     expect(shiritori.wordsList.isEmpty, true);
     expect(shiritori.play('hello'), true);
     expect(shiritori.wordsList.isEmpty, false); // if the word is not preset in the list and valid then add it to the list
+    expect(shiritori.play('hello'), false);//as hello is already present in the list, it should now return false
   });
 }
 
